d73e90a2c744833e9c0a11ec1861265b3d7d37f1
masak <masakkorzhak@gmail.com>
2023-08-21 02:45:32 +0800

    commit in 21.08.2023

--- my_site/news/models.py
+++ my_site/news/models.py
@@ -6,6 +6,8 @@ class News(models.Model):
     title = models.CharField(verbose_name="Заголовок",max_length=50)
     body = models.TextField(verbose_name="Содержание",)
     date = models.DateField(auto_now_add=True)
+    upload_file = models.FileField(upload_to="./uploads/news/videos/%Y/%m/%d/", verbose_name="Добавить видео")
+    upload_img = models.ImageField(upload_to="./uploads/news/images/%Y/%m/%d/", verbose_name="Добавить изображение")
 
     def __str__(self) -> str:
         return self.title
--- my_site/posts/models.py
+++ my_site/posts/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(verbose_name="Заголовок",max_length=50)
     body = models.TextField(verbose_name="Содержание",max_length=500)
     date = models.DateField(auto_now_add=True)
+    upload_file = models.FileField(upload_to="./uploads/posts/files/%Y/%m/%d/", verbose_name="Добавить файл (.pdf)")
+    upload_img = models.ImageField(upload_to="./uploads/posts/imgs/%Y/%m/%d/", verbose_name="Добавить изображение")
     
     def __str__(self) -> str:
         return self.title
@@ -15,12 +18,12 @@ class SubPost(models.Model):
     minitext = models.TextField()
 
 """
-Д/з 
-1 улучшить модель для новостей (в новостях должны быть картинка и видео)
-2 улучшить модель для постов (должна быть картинка и .pdf файл)
-3 Создать подель рейтинга постов
-4 реорганизовать все .html шаблоны и переместить все стили в один файл
-5 продолжать писать про sql
-6 коды состояния http понимать
+Д/з от 18.08.2023
+1 улучшить модель для новостей (в новостях должны быть картинка и видео) +-
+2 улучшить модель для постов (должна быть картинка и .pdf файл) +-
+3 Создать модель рейтинга постов
+4 реорганизовать все .html шаблоны и переместить все стили в один файл +
+5 продолжать писать про sql +
+6 коды состояния http понимать +
 7 протокол tcp/ip
 """
