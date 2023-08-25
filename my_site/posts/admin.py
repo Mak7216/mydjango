07d58ab3d8d0bc23708e403e1d82d06ed434fe40
masak <masakkorzhak@gmail.com>
2023-08-25 18:35:46 +0800

    now may create superuser

--- my_site/posts/admin.py
+++ my_site/posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from posts.models import Post, SubPost
+from posts.models import Post, SubPost, Rating
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(SubPost)
+admin.site.register(Rating)
--- my_site/users/models.py
+++ my_site/users/models.py
@@ -29,6 +29,7 @@ class CustomUser(AbstractBaseUser):
     last_name = models.CharField(max_length=30)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
